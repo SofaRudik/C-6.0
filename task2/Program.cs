5e5039e0c15a1529a2addc3aa06633e599cc07b6
sofia <sonrud@mail.ru>
2022-12-29 19:21:37 +0300

    add task 43

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Задайте значениt b1: ");
+double b1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значениt k1: ");
+double k1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значениt b2: ");
+double b2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значениt k2: ");
+double k2= Convert.ToInt32(Console.ReadLine());
+
+double x=(-b1+b2)/(-k1+k2);
+double y= k2 *x +b2;
+
+Console.WriteLine($"пересечение в (.) с координатами х:{x} и у:{y}");
+
